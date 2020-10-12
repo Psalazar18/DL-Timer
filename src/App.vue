b1f3dbca2fdee9372597c0c900050fc308569c4e
Paola <paolasalazar90@gmail.com>
2020-10-12 14:48:11 -0300

    first commit, done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div id="app">
+    <h3>Cuenta regresiva</h3>
+    <div>
+      {{this.timeLeft}}
+    </div>
+    <ul>
+       <li v-for="(time, index) in times" :key="index">
+         <button @click.prevent="setTime(time.sec)">
+             {{time.display}}
+       </button>
+       </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        data() {
+            return {
+                selectedTime: 0 ,
+                intervalTimer: '',
+                minutes: '',
+                seconds: '',
+                hour: '',
+                timeLeft: '00.00',
+                endTime: '0',
+                times: [
+                    {
+                        sec: 1800,
+                        display: '30 min'
+                    },
+                    {
+                        sec: 600,
+                        display: '10 min'
+                    },
+                    {
+                        sec: 300,
+                        display: '5 min'
+                    },
+                    {
+                         sec: 60,
+                         display: '1 min'
+                    },
+                    {
+                        sec: 3,
+                        display: '3 seg'
+                    },          
+                ]
+            }
+        },
+        methods: {
+          setTime(seconds){
+              clearInterval(this.intervalTimer);
+              this.timer(seconds);
+          },
+          timer(seconds){
+              const now = Date.now();
+              const end = now + seconds * 1000;
+              this.displayTimeLeft(seconds);
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+              this.selectedTime = seconds;
+
+              this.displayEndTime(end);
+              this.countdown(end);
+          },
+          countdown(end){
+              this.intervalTimer = setInterval(()=>{
+                  const secondsLeft = Math.round((end - Date.now()) / 1000);
+
+                  if(secondsLeft === 0 ){
+                      this.endTime = 0;
+                  }
+                  else if(secondsLeft < 0) {
+                      clearInterval(this.intervalTimer);
+                      return;
+                  }
+                  this.displayTimeLeft(secondsLeft)
+              }, 1000);
+          },
+          displayTimeLeft(secondsLeft){
+              this.minutes = Math.floor((secondsLeft % 3600) / 60 );
+              this.seconds = secondsLeft % 60;
+
+              this.timeLeft = `${this.minutes}:${this.seconds}`;
+          },
+          displayEndTime(timestamp){
+            const end = new Date(timestamp);
+            this.hour = end.getHours();
+            this.minutes = end.getMinutes();
+
+            this.endTime = `${this.hour}:${this.minutes}`
+          },
+      },
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button{
+  margin-right: 2em;
 }
 </style>
